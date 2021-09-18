5b8005b002e7c470437c8d7bc6aa2d5135d1e235
18826700 <ALeoKolesnikov@sberbank.ru>
2021-09-18 17:00:40 +0300

    Основное

---
+++ basics.py
@@ -0,0 +1,25 @@
+
+# python3 basics.py
+
+
+
+# Типы данных.
+def output(variable):
+    return print(type(variable))
+
+int = -5 # 1. Целое число.
+output(int)
+float = 5.5 # 2. Дробное число.
+output(float)
+str = 'Hello' # 3. Строка.
+output(str)
+list = [1, 2.3, 'Hello'] # 4. Список.
+output(list)
+dict = {'john': '+7911...'} # 5. Словарь.
+output(dict)
+tuple = (1, 2.3, 'Hello') # 6. Кортеж (последовательность неизменяемых обьектов).
+output(tuple)
+set = {'a', 'b'} # 7. Множество.
+output(set)
+bool = False # 8. Логическое значения.
+output(bool)
